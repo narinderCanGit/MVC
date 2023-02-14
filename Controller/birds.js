2c432736d7c639a23c92d1ef419c3dcc1f070583
narinderCanGit <narinder111198@gmail.com>
2023-02-14 13:17:59 +0530

    Passport local authentication

--- Controller/birds.js
+++
@@ -1,7 +0,0 @@
-exports.home=(req,res)=>{
-    res.render("home", {name:"Birds Home"});
-}
-
-exports.about=(req,res)=>{
-    res.render("about", {name:"Birds About"});
-}
--- index.js
+++ index.js
@@ -1,27 +1,137 @@
-const express = require('express')
-const app = express()
-const path=require ('path');
-const wiki = require ('./Router/wiki');
-const birds = require ('./Router/birds');
-const reqFilter = require ('./Middleware/ageMiddleware');
-app.set('view engine', 'ejs');
+const express			= require('express');
+const session			= require('express-session');
+const hbs				= require('express-handlebars');
+const mongoose			= require('mongoose');
+const passport			= require('passport');
+const localStrategy		= require('passport-local').Strategy;
+const bcrypt			= require('bcrypt');
+const app				= express();
 
-app.use(reqFilter);
+mongoose.connect("mongodb://localhost:27017/node-auth", {
+	 useNewUrlParser: true,
+	 useUnifiedTopology: true
+});
 
-app.use('/wiki', wiki);
-app.use('/birds', birds);
-app.get('/*', (req,res)=>{
-    res.send('<h1>Page Not Found!!</h1>');
-})
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
 
-// app.get('/',(req,res)=>{
-//     res.send("Welcome to Express");
-// });
+const User = mongoose.model('User', UserSchema);
 
-// app.get('/about', function(req,res){
-//     res.sendFile(path.join(__dirname+'/dummy.html'));
-// })
 
-app.listen(8000,()=>{
-    console.log("Server is running on port 8000");
-})
+// Middleware
+app.engine('hbs', hbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+	secret: "verygoodsecret",
+	resave: false,
+	saveUninitialized: true
+}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+passport.use(new localStrategy(function (username, password, done) {
+	User.findOne({ username: username }, function (err, user) {
+		if (err) return done(err);
+		if (!user) return done(null, false, { message: 'Incorrect username.' });
+
+		bcrypt.compare(password, user.password, function (err, res) {
+			if (err) return done(err);
+			if (res === false) return done(null, false, { message: 'Incorrect password.' });
+			
+			return done(null, user);
+		});
+	});
+}));
+
+
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) return next();
+	res.redirect('/login');                    //2
+}
+
+function isLoggedOut(req, res, next) {
+	if (!req.isAuthenticated()) return next();
+	res.redirect('/');
+}
+
+// ROUTES
+app.get('/', isLoggedIn, (req, res) => {             //1
+	res.render("index", { title: "Home" });         //5
+});
+
+app.get('/about', (req, res) => {
+	res.render("index", { title: "About" });
+});
+
+app.get('/login', isLoggedOut, (req, res) => {   //3 display login page
+	const response = {
+		title: "Login",
+		error: req.query.error
+	}
+
+	res.render('login', response);
+});
+
+app.post('/login', passport.authenticate('local', {   //4 
+	successRedirect: '/',
+	failureRedirect: '/login?error=true'
+}));
+
+app.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+// Setup our admin user
+app.get('/setup', async (req, res) => {      // works only when admin is not in the DB
+	const exists = await User.exists({ username: "admin" });
+
+	if (exists) {
+		res.redirect('/login');
+		return;
+	};
+
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) return next(err);
+		bcrypt.hash("pass", salt, function (err, hash) {
+			if (err) return next(err);
+			
+			const newAdmin = new User({
+				username: "admin",
+				password: hash
+			});
+
+			newAdmin.save();
+
+			res.redirect('/login');
+		});
+	});
+});
+
+app.listen(3000, () => {
+	console.log("Listening on port 3000");
+});
